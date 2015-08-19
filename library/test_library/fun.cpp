9d7c0e56c67554e3401320d83e316e4c59b08705
Dmitry Kuznetsov <kuznetsov@avast.com>
2015-08-19 17:53:49 +0200

    library example

---
+++ library/test_library/fun.cpp
@@ -0,0 +1,9 @@
+#include "fun.h"
+
+#include <iostream>
+
+
+void f() {
+    std::cout << "fun\n";
+}
+
--- test_project/main.cpp
+++ test_project/main.cpp
@@ -1,7 +1,10 @@
+#include <library/test_library/fun.h>
+
 #include <iostream>
 
 
 int main() {
     std::cout << "Hello!\n";
+    f();
 }
 
